69e664cd04c1ebf057f7a44b6f8bf336d0e4b092
Erik Herazo <erickherazoj@gmail.com>
2023-06-01 14:14:12 -0500

    primer endpoin de prueba

--- flaskApps/apps/__init__.py
+++ flaskApps/apps/__init__.py
@@ -1,8 +1,17 @@
+import redis
 from flask import Flask
 from flask_restful import Api
+from apps.user.views import UserRestFull
 
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(UserRestFull, '/useRegistration')
+
+redisConnection = redis.Redis(
+  host='redis-10179.c14.us-east-1-3.ec2.cloud.redislabs.com',
+  port=10179,
+  password='ingeniero010')
 
 from apps.user.views import user
 
--- flaskApps/apps/user/views.py
+++ flaskApps/apps/user/views.py
@@ -1,11 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource
-from apps import api
+# from apps import redisConnection
 
 user = Blueprint('user', __name__)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+class UserRestFull(Resource):
+    def post(self):
+        data = request.json
+        print("holaaaaaa......................")
+        print(type(data))
+        # response = redisConnection.set()
 
-api.add_resource(HelloWorld, '/')
+    # def abort_if_todo_doesnt_exist(self, id):
+    #     if todo_id not in TODOS:
+    #         abort(404, message="Todo {} doesn't exist".format(todo_id))
